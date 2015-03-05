632646622c39c7be7498f1268e3b62f9c32973c2
Joseph Murphy III <jmrphyii@memphis.edu>
2015-03-05 15:27:38 -0600

    new access

--- EstablishAccess.php
+++ EstablishAccess.php
@@ -1,8 +1,8 @@
 <?php
 
 define ("DB_USER", "BetaPoints");
-define ("DB_PASSWORD", "Mybirthday613!");
-define ("DB_DATABASE", "BetaPointss");
-define ("DB_HOST", "BetaPoints.db.8274306.hostedresource.com");
+define ("DB_PASSWORD", "password");
+define ("DB_DATABASE", "BetaPoints");
+define ("DB_HOST", "Host");
 
 ?>
